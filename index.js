b1c13149ada53d8cd986c8ee7b84b928750ced47
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-09-28 08:14:05 +1000

    feat: Render sky background with start in the middle

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static("."));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
