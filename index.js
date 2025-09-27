eeaaadcc496c8adc2efad61c2125cf91fbe11e7b
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-09-27 19:41:59 +1000

    Display basic phaser starting point in HTML file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Phaser Project!");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
